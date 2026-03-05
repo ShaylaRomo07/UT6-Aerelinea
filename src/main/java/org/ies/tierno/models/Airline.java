13cb81db0798a10c5b1104f55a37029429ca6c30
ShaylaRomo07 <sarcclases@gmail.com>
2026-03-05 11:52:32 +0100

    No se

--- src/main/java/org/ies/tierno/models/Airline.java
+++ src/main/java/org/ies/tierno/models/Airline.java
@@ -3,6 +3,7 @@ package org.ies.tierno.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,10 +16,8 @@ public class Airline {
     private List<Customer> customers;
 
     public List<Passenger> passengersFromAFlight(int flightNumber) {
-        return flightsByFlightNumber.get(flightNumber).getPassengerBySeatNumber().values()
-                .stream()
-                .collect(Collectors.toList());
+        return flightsByFlightNumber.get(flightNumber).getPassengerBySeatNumber().values().stream().collect(Collectors.toList());
     }
 
-    public
+//    public List<Customer> customerInFlight
 }
