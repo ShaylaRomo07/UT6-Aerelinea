a26220cae6b48db5f0fa823d21f4cf239af43210
ShaylaRomo07 <sarcclases@gmail.com>
2026-03-06 08:40:29 +0100

    Ya puse comparable faltan metodos y si eso luego revisar si falta algo y si funcionan los metodos

--- src/main/java/org/ies/tierno/models/Passenger.java
+++ src/main/java/org/ies/tierno/models/Passenger.java
@@ -3,28 +3,22 @@ package org.ies.tierno.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Comparator;
+
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class Passenger implements Comparable<Passenger>{
+public class Passenger implements Comparable<Passenger> {
     private String nif;
     private Integer seatNumber;
     private List<Luggage> luggages;
 
     @Override
-    public int compare(Passenger o1, Passenger o2) {
-        int compare = o1.getNif().compareTo(o2.getNif());
-        if (compare==0){
-            compare = o1.getSeatNumber().compareTo(o2.getSeatNumber());
-            return compare;
+    public int compareTo(Passenger passenger) {
+        int compare = nif.compareTo(passenger.getNif());
+        if (compare == 0) {
+            compare = Integer.compare(this.seatNumber, passenger.getSeatNumber());
         }
         return compare;
-    }
-
-    @Override
-    public int compareTo(Passenger passenger) {
-        return 0;
     }
 }
